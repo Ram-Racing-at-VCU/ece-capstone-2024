e0a480ed9004ca1c9e01165debbff56a179ec797
Nathan Rowan <60853993+natrow@users.noreply.github.com>
2023-10-30 18:56:30 -0400

    3-phase w/ complementary configuration

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -2,18 +2,19 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use cortex_m::prelude::_embedded_hal_Pwm;
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_stm32::{
     gpio::OutputType,
-    rcc, time,
+    interrupt, rcc, time,
     timer::{
-        simple_pwm::{PwmPin, SimplePwm},
+        complementary_pwm::{ComplementaryPwm, ComplementaryPwmPin},
+        simple_pwm::PwmPin,
         Channel, CountingMode,
     },
     Config,
 };
+use embedded_hal_02::Pwm;
 
 use {defmt_rtt as _, panic_probe as _}; // logger & panic handler
 
@@ -37,36 +38,77 @@ async fn main(_s: Spawner) {
 
     let p = embassy_stm32::init(config);
 
+    let ch1 = PwmPin::new_ch1(p.PA8, OutputType::PushPull);
+    let ch1n = ComplementaryPwmPin::new_ch1(p.PA7, OutputType::PushPull);
     let ch2 = PwmPin::new_ch2(p.PA9, OutputType::PushPull);
-    let mut servo = SimplePwm::new(
+    let ch2n = ComplementaryPwmPin::new_ch2(p.PA12, OutputType::PushPull);
+    let ch3 = PwmPin::new_ch3(p.PA10, OutputType::PushPull);
+    let ch3n = ComplementaryPwmPin::new_ch3(p.PF0, OutputType::PushPull);
+
+    let mut motor = ComplementaryPwm::new(
         p.TIM1,
-        None,
+        Some(ch1),
+        Some(ch1n),
         Some(ch2),
+        Some(ch2n),
+        Some(ch3),
+        Some(ch3n),
         None,
         None,
-        time::mhz(1),
-        CountingMode::EdgeAlignedUp,
+        time::hz(5), // 25 kHz / 2 (center aligned)
+        CountingMode::CenterAlignedBothInterrupts,
     );
+
+    // enable update interrupt
+    embassy_stm32::pac::TIM1.dier().modify(|dier| {
+        dier.set_uie(true);
+    });
 
     info!("PWM Initialized!");
 
-    servo.set_period(time::hz(60));
+    motor.set_duty(Channel::Ch1, 0);
+    motor.set_duty(Channel::Ch2, 0);
+    motor.set_duty(Channel::Ch3, 0);
+
+    motor.enable(Channel::Ch1);
+    motor.enable(Channel::Ch2);
+    motor.enable(Channel::Ch3);
+
+    info!("Motor period: {}", motor.get_period());
+
+    // servo.set_period(time::hz(60));
 
     // calculate duty cycle
-    let period = 1000.0 / (servo.get_period().0 as f32); // period in ms
-    let duty = 1.5; // duty in ms
+    // let period = 1000.0 / (servo.get_period().0 as f32); // period in ms
+    // let duty = 1.5; // duty in ms
 
-    let duty_frac = duty / period; // duty % (0..1)
-    let div = (1.0 / duty_frac) as u16; // divider
+    // let duty_frac = duty / period; // duty % (0..1)
 
-    let max = servo.get_max_duty();
-    let duty = max / div;
-
-    servo.set_duty(Channel::Ch2, duty);
-
-    servo.enable(Channel::Ch2);
+    // set_duty_cycle(&mut servo, duty_frac, Channel::Ch2);
+    // servo.enable(Channel::Ch2);
 
     loop {
         cortex_m::asm::wfi(); // sleep
     }
+}
+
+/// Set duty cycle given a fraction from 0 to 1 (inclusive)
+fn set_duty_cycle<T>(servo: &mut T, frac: f32, channel: T::Channel)
+where
+    T: embedded_hal_02::Pwm,
+    T::Duty: From<f32> + core::ops::Div<T::Duty, Output = T::Duty>,
+{
+    let div: T::Duty = (1.0 / frac).into();
+
+    let max = servo.get_max_duty();
+
+    let duty = max / div;
+
+    servo.set_duty(channel, duty);
+}
+
+#[interrupt]
+fn TIM1_UP_TIM16() {
+    // todo: fix receiving interrupts, generate next duty cycle value & update in real-time
+    info!("recieved interrupt");
 }
