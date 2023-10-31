7c6bd8fcfbf4c095bd1402c4e5c2f82103a099c5
cas101101 <caleb.a.schwartz@gmail.com>
2023-10-31 12:04:26 -0400

    Yaqub changed stuff and said caleb was bad at coding

--- SPWM.py
+++ SPWM.py
@@ -63,9 +63,9 @@ def phi(f, R, L):
 #***********************************#
 
 T0 = 0.02
-f_c = 25e3
+f_c = 37.8e3
 f_m = 50
-dt = 1e-6
+dt = 1e-7
 
 t = arange(0, T0, dt)
 carrier_signal = 1/2 * signal.sawtooth(2*pi*f_c*t, width=0.5) + 1/2 # adding 1/2 to go from 1 to 0
@@ -101,42 +101,9 @@ plt.legend(["SPWM Signal"])
 dc_component = 1/T0 * integral(spwm_signal, dt)
 coeffRange = 1000
 harmRange = 100
-coeff = []
 
-for n in range(0, coeffRange):
-    res_list = spwm_signal * cos(2*pi*f_m*n*t)
-    test = integral(res_list, dt)
-    coeff.append(2/T0 * test)
-harmonics = np.array(dc_component)
+coeff = np.array([2/T0 * integral(spwm_signal * cos(2*pi*f_m*n*t), dt) for n in range(0, coeffRange)])
 
-
-# Calculating and plotting the first 100 harmonics
-for n in range(1, harmRange):
-    harmonics =  harmonics + coeff[n] * cos(2*pi*n*f_m*t)
-
-harmRange = 1000
-harmonics_1000 = np.array(dc_component)
-# Calculating and plotting the first 1000 harmonics
-for n in range(1, harmRange):
-    harmonics_1000 =  harmonics_1000 + coeff[n] * cos(2*pi*n*f_m*t)
-
-
-firstCoeff = np.empty(len(t))
-count = 0
-for time in t:
-    firstCoeff[count] = (coeff[1]*cos(2*pi*f_m*time) + dc_component)
-    count += 1
-
-fig, axs = plt.subplots(2, 2)
-
-axs[0, 0].plot(1000*t, spwm_signal, linewidth='0.5')
-axs[0, 0].set_title('SPWM Signal')
-axs[0, 1].plot(1000*t, firstCoeff, 'tab:orange', linewidth='0.5')
-axs[0, 1].set_title('First Coefficient')
-axs[1, 0].plot(1000*t, harmonics, 'tab:green', linewidth='0.5')
-axs[1, 0].set_title('The First 100 Harmonics')
-axs[1, 1].plot(1000*t, harmonics_1000, 'tab:red', linewidth='0.5')
-axs[1, 1].set_title('The First 1000 Harmonics')
 
 #***********************************#
 #             PART 3                #
@@ -148,19 +115,6 @@ axs[1, 1].set_title('The First 1000 Harmonics')
 L = 5.73e-6
 R = 0.0932
 
-f = np.linspace(0,1000)
-
-M = magnitude(f, R, L)
-P = phi(f, R, L)
-
-fig, axs = plt.subplots(1, 2)
-
-fig.suptitle(f'Simulation with L = {L} H and R = {R} $\Omega$')
-
-axs[0].plot(f, M, linewidth='0.5')
-axs[0].set_title('Magnitude')
-axs[1].plot(f, 180/pi*P, 'tab:orange', linewidth='0.5')
-axs[1].set_title('Phase (deg)')
 
 # plt.figure()
 # plt.plot(f, M)
@@ -179,18 +133,11 @@ axs[1].set_title('Phase (deg)')
 #             PART 4                #
 #***********************************#
 
-out_sig_1 = 0
-for n in range(0,coeffRange):
-    out_sig_1 = out_sig_1 + magnitude((n+1)*f_m, R, L)*coeff[n]*cos(2*pi*n*f_m*t + phi(n*f_m, R, L))
-
-plt.figure()
-plt.plot(1000*t, out_sig_1)
-plt.title(f"Output simulation with R = {R} $\Omega$ and L = {L} H")
 
 thing = 0
 for n in range(2, coeffRange):
     thing += (magnitude(n*f_m, R, L)*coeff[n])**2
-thd = sqrt(thing)/coeff[1] * magnitude(f_m, R, L)
+thd = sqrt(thing)/(coeff[1] * magnitude(f_m, R, L))
 print(thd)
 
 # L = 5.73e-6
